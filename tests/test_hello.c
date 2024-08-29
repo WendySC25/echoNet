d59d843d139558f452896b4b13f5375064061257
WendySC <wendy.c2502@gmail.com>
2024-08-29 16:41:11 -0600

    Initial commit

---
+++ tests/test_hello.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+void setUp(void) {
+    // Se llama antes de cada prueba
+}
+
+void tearDown(void) {
+    // Se llama después de cada prueba
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    //RUN_TEST();
+    return UNITY_END();
+}
+
