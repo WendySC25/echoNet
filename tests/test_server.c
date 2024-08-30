b8109a25810c973373f9e9bac580867e074bf62a
WendySC <wendy.c2502@gmail.com>
2024-08-30 08:25:10 -0600

    Add test: test_socket_connection

--- tests/test_server.c
+++ tests/test_server.c
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define MAX_USERNAME_LENGTH 8
 
 //Clases temporales para hacer pruebas unitarias
-
 typedef enum {
     ONLINE,
     AWAY,
@@ -22,14 +23,46 @@ typedef struct {
     int socketFD;      
 } User;
 
-
+User createUser(const char *username, UserStatus status, int socketFD) {
+    User user;
+    strncpy(user.username, username, MAX_USERNAME_LENGTH);
+    user.username[MAX_USERNAME_LENGTH] = '\0';
+    user.status = status;
+    user.socketFD = socketFD;
+    return user;
+}
 
 void setUp(void) {
-    // Se llama antes de cada prueba
+    // stuff
+
 }
 
 void tearDown(void) {
-    // Se llama después de cada prueba
+    // stuff
+}
+
+void test_socket_connection(void) {
+
+    int clientSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+    TEST_ASSERT_NOT_EQUAL(-1, clientSocketFD); 
+
+    struct sockaddr_in *clientAddress = malloc(sizeof(struct sockaddr_in));
+    TEST_ASSERT_NOT_NULL(clientAddress); 
+
+    int port = 2000;
+    char *ip = "127.0.0.1";
+    clientAddress->sin_family = AF_INET;
+    clientAddress->sin_port = htons(port);
+
+    int inet_pton_result = inet_pton(AF_INET, ip, &clientAddress->sin_addr.s_addr);
+    TEST_ASSERT_EQUAL(1, inet_pton_result); 
+
+    int connect_result = connect(clientSocketFD, (struct sockaddr *)clientAddress, sizeof(*clientAddress));
+    TEST_ASSERT_EQUAL(0, connect_result); 
+
+    free(clientAddress); 
+    close(clientSocketFD); 
+    
 }
 
 
@@ -37,42 +70,8 @@ int main(void) {
 
     UNITY_BEGIN();
 
-    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    RUN_TEST(test_socket_connection);
 
-    User userA;
-    strncpy(userA.username, "Alice", sizeof(userA.username) - 1);
-    userA.username[sizeof(userA.username) - 1] = '\0'; 
-    userA.status = ONLINE;
-    userA.socketFD = socketFD;
-
-    struct sockaddr_in *address = malloc(sizeof(struct sockaddr_in));
-    int port  = 2000;
-    char *ip = "127.0.0.1";
-    address->sin_family = AF_INET;
-    address->sin_port = htons(port); 
-
-    if (inet_pton(AF_INET, ip, &address->sin_addr.s_addr) != 1) {
-            perror("inet_pton failed");
-            free(address); 
-            exit(EXIT_FAILURE);
-    }   
-
-    int result = connect(socketFD, (struct sockaddr *)address, sizeof(*address));
-    free(address);
-
-    if (result == 0)
-        printf("Connection was successful\n");
-    else {
-        printf("Connection fail\n");
-        exit(EXIT_FAILURE);
-    }
-
-    close(socketFD);
-
-
-
-
-    //RUN_TEST();
     return UNITY_END();
 }
 
