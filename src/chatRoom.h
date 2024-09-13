4476885419af844214b8f3c3a7c686eb2b86a210
WendySC <wendy.c2502@gmail.com>
2024-09-12 19:38:25 -0600

    feat - Added chat room struct

---
+++ src/chatRoom.h
@@ -0,0 +1,18 @@
+// #ifndef CHATROOM_H
+// #define CHATROOM_H
+
+// #include <glib.h>
+
+// typedef struct {
+//     char room_name[16];       // Nombre de la sala
+//     GHashTable *clients;      // Hash table de clientes en la sala
+
+
+// } ChatRoom;
+
+// ChatRoom* createChatRoom(const gchar *roomname, const gchar *creator_username);
+// void freeChatRoom(ChatRoom *chatRoom)
+
+
+// #endif
+
